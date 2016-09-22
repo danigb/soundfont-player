1a7cf96b417a7039f1866bc1d03e88ac4283de92
danigb <danigb@gmail.com>
2016-09-22 11:45:43 +0200

    bump 0.10.2

--- dist/soundfont-player.js
+++ dist/soundfont-player.js
@@ -18,7 +18,7 @@ var player = require('sample-player')
  * The valid options are:
  *
  * - `format`: the soundfont format. 'mp3' by default. Can be 'ogg'
- * - `soundfont`: the soundfont name. 'FluidR3_GM' by default. Can be 'MusyngKite'
+ * - `soundfont`: the soundfont name. 'MusyngKite' by default. Can be 'FluidR3_GM'
  * - `nameToUrl` <Function>: a function to convert from instrument names to URL
  * - `destination`: by default Soundfont uses the `audioContext.destination` but you can override it.
  * - `gain`: the gain of the player (1 by default)
@@ -73,7 +73,7 @@ function isSoundfontURL (name) {
 function nameToUrl (name, sf, format) {
   format = format === 'ogg' ? format : 'mp3'
   sf = sf === 'FluidR3_GM' ? sf : 'MusyngKite'
-  return 'http://gleitz.github.io/midi-js-soundfonts/' + sf + '/' + name + '-' + format + '.js'
+  return 'https://gleitz.github.io/midi-js-soundfonts/' + sf + '/' + name + '-' + format + '.js'
 }
 
 // In the 1.0.0 release it will be:
@@ -443,6 +443,15 @@ if (typeof window !== 'undefined') window.loadAudio = load
 },{"./base64":3,"./fetch":4}],6:[function(require,module,exports){
 'use strict'
 
+// util
+function fillStr (s, num) { return Array(num + 1).join(s) }
+function isNum (x) { return typeof x === 'number' }
+function isStr (x) { return typeof x === 'string' }
+function isDef (x) { return typeof x !== 'undefined' }
+function midiToFreq (midi, tuning) {
+  return Math.pow(2, (midi - 69) / 12) * (tuning || 440)
+}
+
 var REGEX = /^([a-gA-G])(#{1,}|b{1,}|x{1,}|)(-?\d*)\s*(.*)\s*$/
 /**
  * A regex for matching note strings in scientific notation.
@@ -488,8 +497,9 @@ var SEMITONES = [0, 2, 4, 5, 7, 9, 11]
  * @name parse
  * @function
  * @param {String} note - the note string to be parsed
- * @param {Boolean} isTonic - true if the note is the tonic of something.
- * If true, en extra tonicOf property is returned. It's false by default.
+ * @param {Boolean} isTonic - true the strings it's supposed to contain a note number
+ * and some category (for example an scale: 'C# major'). It's false by default,
+ * but when true, en extra tonicOf property is returned with the category ('major')
  * @param {Float} tunning - The frequency of A4 note to calculate frequencies.
  * By default it 440.
  * @return {Object} the parsed note name or null if not a valid note
@@ -503,8 +513,8 @@ var SEMITONES = [0, 2, 4, 5, 7, 9, 11]
  * - alt: a numeric representation of the accidentals. 0 means no alteration,
  * positive numbers are for sharps and negative for flats
  * - chroma: a numeric representation of the pitch class. It's like midi for
- * pitch classes. 0 = C, 1 = C#, 2 = D ... It can have negative values: -1 = Cb.
- * Can detect pitch class enhramonics.
+ * pitch classes. 0 = C, 1 = C#, 2 = D ... 11 = B. Can be used to find enharmonics
+ * since, for example, chroma of 'Cb' and 'B' are both 11
  *
  * If the note has octave, the parser object will contain:
  * - oct: the octave number (as integer)
@@ -532,64 +542,102 @@ function parse (str, isTonic, tuning) {
   p.pc = p.letter + p.acc
   p.step = (p.letter.charCodeAt(0) + 3) % 7
   p.alt = p.acc[0] === 'b' ? -p.acc.length : p.acc.length
-  p.chroma = SEMITONES[p.step] + p.alt
-  if (m[3]) {
+  var pos = SEMITONES[p.step] + p.alt
+  p.chroma = pos < 0 ? 12 + pos : pos % 12
+  if (m[3]) { // has octave
     p.oct = +m[3]
-    p.midi = p.chroma + 12 * (p.oct + 1)
+    p.midi = pos + 12 * (p.oct + 1)
     p.freq = midiToFreq(p.midi, tuning)
   }
   if (isTonic) p.tonicOf = m[4]
   return p
 }
 
+var LETTERS = 'CDEFGAB'
+function acc (n) { return !isNum(n) ? '' : n < 0 ? fillStr('b', -n) : fillStr('#', n) }
+function oct (n) { return !isNum(n) ? '' : '' + n }
+
 /**
- * Given a midi number, return its frequency
- * @param {Integer} midi - midi note number
- * @param {Float} tuning - (Optional) the A4 tuning (440Hz by default)
- * @return {Float} frequency in hertzs
+ * Create a string from a parsed object or `step, alteration, octave` parameters
+ * @param {Object} obj - the parsed data object
+ * @return {String} a note string or null if not valid parameters
+ * @since 1.2
+ * @example
+ * parser.build(parser.parse('cb2')) // => 'Cb2'
+ *
+ * @example
+ * // it accepts (step, alteration, octave) parameters:
+ * parser.build(3) // => 'F'
+ * parser.build(3, -1) // => 'Fb'
+ * parser.build(3, -1, 4) // => 'Fb4'
  */
-function midiToFreq (midi, tuning) {
-  return Math.pow(2, (midi - 69) / 12) * (tuning || 440)
+function build (s, a, o) {
+  if (s === null || typeof s === 'undefined') return null
+  if (s.step) return build(s.step, s.alt, s.oct)
+  if (s < 0 || s > 6) return null
+  return LETTERS.charAt(s) + acc(a) + oct(o)
 }
 
-var parser = { parse: parse, regex: regex, midiToFreq: midiToFreq }
-var FNS = ['letter', 'acc', 'pc', 'step', 'alt', 'chroma', 'oct', 'midi', 'freq']
-FNS.forEach(function (name) {
-  parser[name] = function (src) {
-    var p = parse(src)
-    return p && (typeof p[name] !== 'undefined') ? p[name] : null
-  }
-})
-
-module.exports = parser
-
-// extra API docs
 /**
  * Get midi of a note
  *
  * @name midi
  * @function
- * @param {String} note - the note name
+ * @param {String|Integer} note - the note name or midi number
  * @return {Integer} the midi number of the note or null if not a valid note
  * or the note does NOT contains octave
  * @example
  * var parser = require('note-parser')
  * parser.midi('A4') // => 69
  * parser.midi('A') // => null
+ * @example
+ * // midi numbers are bypassed (even as strings)
+ * parser.midi(60) // => 60
+ * parser.midi('60') // => 60
  */
+function midi (note) {
+  if ((isNum(note) || isStr(note)) && note >= 0 && note < 128) return +note
+  var p = parse(note)
+  return p && isDef(p.midi) ? p.midi : null
+}
+
 /**
  * Get freq of a note in hertzs (in a well tempered 440Hz A4)
  *
  * @name freq
  * @function
- * @param {String} note - the note name
+ * @param {String} note - the note name or note midi number
+ * @param {String} tuning - (Optional) the A4 frequency (440 by default)
  * @return {Float} the freq of the number if hertzs or null if not valid note
- * or the note does NOT contains octave
  * @example
  * var parser = require('note-parser')
  * parser.freq('A4') // => 440
  * parser.freq('A') // => null
+ * @example
+ * // can change tuning (440 by default)
+ * parser.freq('A4', 444) // => 444
+ * parser.freq('A3', 444) // => 222
+ * @example
+ * // it accepts midi numbers (as numbers and as strings)
+ * parser.freq(69) // => 440
+ * parser.freq('69', 442) // => 442
  */
+function freq (note, tuning) {
+  var m = midi(note)
+  return m === null ? null : midiToFreq(m, tuning)
+}
+
+var parser = { parse: parse, build: build, regex: regex, midi: midi, freq: freq }
+// add additional functions, one for each object property
+var FNS = ['letter', 'acc', 'pc', 'step', 'alt', 'chroma', 'oct']
+FNS.forEach(function (name) {
+  parser[name] = function (src) {
+    var p = parse(src)
+    return p && isDef(p[name]) ? p[name] : null
+  }
+})
+
+module.exports = parser
 
 },{}],7:[function(require,module,exports){
 
